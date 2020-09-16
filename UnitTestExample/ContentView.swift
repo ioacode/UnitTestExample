010eeb0992835d68b3efe34001eba281218bc8ce
shiddiq <ahmadshiddiq0@gmail.com>
2020-09-16 07:11:31 +0700

    Initial Commit

---
+++ UnitTestExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitTestExample
+//
+//  Created by ahmad shiddiq on 16/09/20.
+//  Copyright © 2020 ahmad shiddiq. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
